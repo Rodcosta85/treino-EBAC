dd0726d2a6e767210f92e2565561ebf580e2d03f
Rodcosta85 <147288678+Rodcosta85@users.noreply.github.com>
2025-08-20 19:12:12 -0300

    mais alterações de js

--- SEVEN/exercise-seven.js
+++ SEVEN/exercise-seven.js
@@ -3,36 +3,39 @@ document.addEventListener('DOMContentLoaded', function () {
     const minutosSpan = document.getElementById('minutos');
     const resultadoP = document.getElementById('resultado');
 
-    const valoresMin = {
-        1:30,
-        1.75:60,
-        3:120
-    }
-
 
     const calculateTimeAndChange = (e) => {
-        e.preventDefault();
+    e.preventDefault();
 
-        const inputValue = parseFloat(document.getElementById('valor-input').value);
-        let timeLeft = 0;
+    const inputValue = parseFloat(document.getElementById('valor-input').value);
+    let timeLeft = 0;
 
-        if (inputValue < 1) {
-           resultadoP.textContent = 'Valor insuficiente';
-        } else if (inputValue === 1) {
-            minutosSpan.textContent = 30;
-        } else if (inputValue === 1.75) {
-            minutosSpan.textContent = 60;
-        } else if (inputValue === 3) {
-            minutosSpan.textContent = 120;
-        } else if (inputValue !== Object.keys(valoresMin) && 1 < inputValue < 1.75) {
-            timeLeft = 30 + (60-30) * (inputValue - 1)/(1.75 - 1);
-            console.log(timeLeft);
-            minutosSpan.textContent = timeLeft;
-        } else if (inputValue !== Object.keys(valoresMin) && 1.75 < inputValue < 3) {
-            timeLeft = 60 + (120 - 60) * (inputValue - 1.75)/(3 - 1.75);
-            minutosSpan.textContent = timeLeft;
-        }
+    if (inputValue < 1) {
+        resultadoP.textContent = 'Valor insuficiente';
+        minutosSpan.textContent = 0;
+
+    } else if (inputValue === 1) {
+        minutosSpan.textContent = 30;
+
+    } else if (inputValue === 1.75) {
+        minutosSpan.textContent = 60;
+
+    } else if (inputValue === 3) {
+        minutosSpan.textContent = 120;
+
+    } else if (inputValue > 1 && inputValue < 1.75) {
+        timeLeft = 30 + (60 - 30) * (inputValue - 1) / (1.75 - 1);
+        minutosSpan.textContent = Math.round(timeLeft);
+
+    } else if (inputValue > 1.75 && inputValue < 3) {
+        timeLeft = 60 + (120 - 60) * (inputValue - 1.75) / (3 - 1.75);
+        minutosSpan.textContent = Math.round(timeLeft);
+
+    } else if (inputValue > 3) {
+        minutosSpan.textContent = 120;
+        resultadoP.textContent = 'Valor máximo é $3';
     }
+}
 
     calcularBtn.addEventListener('click', calculateTimeAndChange);
 
