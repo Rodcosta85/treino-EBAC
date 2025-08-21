4b119319f4d318e80bae662c854a6a44be609551
Rodcosta85 <147288678+Rodcosta85@users.noreply.github.com>
2025-08-21 11:23:11 -0300

    mais mudanças no POO e uma no html

--- SEVEN/exercise-seven.js
+++ SEVEN/exercise-seven.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     class ParkingCalculator {
-        constructor(calcularBtnId, minutosSpanId, resultadoPId, inputId) {
+        constructor(calcularBtnId, resultadoPId, inputId, trocoPId) {
             this.calcularBtn = document.getElementById(calcularBtnId);
-            this.minutosSpan = document.getElementById(minutosSpanId);
             this.resultadoP = document.getElementById(resultadoPId);
             this.input = document.getElementById(inputId);
+            this.trocoP = document.getElementById(trocoPId);
 
             this.init();
         }
@@ -23,45 +23,47 @@ document.addEventListener('DOMContentLoaded', () => {
 
             if (isNaN(inputValue)) {
                 this.resultadoP.textContent = 'Digite um valor válido';
-                this.minutosSpan.textContent = 0;
                 return;
             }
 
             if (inputValue < 1) {
-                this.resultadoP.textContent = 'Valor insuficiente';
-                this.minutosSpan.textContent = 0;
+                this.resultadoP.textContent = 'Valor insuficiente, você não pode estacionar';
+                this.trocoP.textContent = '';
 
             } else if (inputValue === 1) {
-                this.minutosSpan.textContent = 30;
+                this.resultadoP.textContent = 'Você tem 30 minutos de estacionamento'
+                this.trocoP.textContent = '';
 
             } else if (inputValue === 1.75) {
-                this.minutosSpan.textContent = 60;
+                this.resultadoP.textContent = 'Você tem 60 minutos de estacionamento'
+                this.trocoP.textContent = '';
 
             } else if (inputValue === 3) {
-                this.minutosSpan.textContent = 120;
+                this.resultadoP.textContent = 'Você tem 120 minutos de estacionamento'
+                this.trocoP.textContent = '';
 
             } else if (inputValue > 1 && inputValue < 1.75) {
                 timeLeft = 30 + (60 - 30) * (inputValue - 1) / (1.75 - 1);
-                this.minutosSpan.textContent = Math.round(timeLeft);
                 const troco = (inputValue - 1).toFixed(2);
-                this.resultadoP.textContent = `Seu troco: $${troco}`;
+                this.resultadoP.textContent = 'Você tem 30 minutos de estacionamento'
+                this.trocoP.textContent = `Seu troco: $${troco}`;
 
             } else if (inputValue > 1.75 && inputValue < 3) {
                 timeLeft = 60 + (120 - 60) * (inputValue - 1.75) / (3 - 1.75);
-                this.minutosSpan.textContent = Math.round(timeLeft);
                 const troco = (inputValue - 1.75).toFixed(2);
-                this.resultadoP.textContent = `Seu troco: $${troco}`;
+                this.resultadoP.textContent = 'Você tem 60 minutos de estacionamento'
+                this.trocoP.textContent = `Seu troco: $${troco}`;
 
             } else if (inputValue > 3) {
-                this.minutosSpan.textContent = 120;
                 const troco = (inputValue - 3).toFixed(2);
-                this.resultadoP.textContent = `Valor máximo é $3. Seu troco: $${troco}`;
+                this.resultadoP.textContent = 'Você tem 120 minutos de estacionamento'
+                this.trocoP.textContent = `Seu troco: $${troco}`;
             }
         }
     }
 
     // instantiate
-    new ParkingCalculator('calcular-btn', 'minutos', 'resultado', 'valor-input');
+    new ParkingCalculator('calcular-btn', 'resultado', 'valor-input', 'troco-p');
 });
 
 
